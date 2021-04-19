86da0ec19d50bb1ec11f0c9c4165121e8d46c6b4
DanielHearn <hearndaniel@live.co.uk>
2021-04-19 20:37:59 +0100

    Add player detection and look at

--- fps/assets/Target/Target.gd
+++ fps/assets/Target/Target.gd
@@ -1,13 +1,28 @@
-extends KinematicBody
+extends RigidBody
 
 signal hit
 
 export var health = 100
-
-
+var detectedPlayer = false
 
 func _on_Target_hit(data):
 	print('target hit')
-	health -= data.damage
-	if(health < 0):
-		queue_free()
+	if(data.damage):
+		health -= data.damage
+		detectedPlayer = true
+		if(health < 0):
+			queue_free()
+
+func _process(delta):
+	if detectedPlayer:
+		var player = get_node("../Player")
+		if player and player.playable:
+			var endposition = get_node("../Player").global_transform.origin;
+			look_at(Vector3(endposition.x, translation.y, endposition.z), Vector3(0, 1, 0))
+
+
+func _on_detection_zone_body_entered(body):
+	if body.is_in_group("player"):
+		detectedPlayer = true
+		print('Player detected')
+		
