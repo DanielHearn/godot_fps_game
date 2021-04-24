5ae0f55a31b9894823b1ef3459376745eff147a7
DanielHearn <hearndaniel@live.co.uk>
2021-04-24 15:38:51 +0100

    Fix door with multiple colliding entities

--- fps/assets/Door/Door.gd
+++ fps/assets/Door/Door.gd
@@ -1,25 +1,25 @@
 extends Spatial
 
-var open = false
 var y = 0
 var maxOpen = 1.8
 var speed = 3
+var colliding_entities = 0
 
 func _process(delta):
 	var delta_speed = speed * delta
-	if open and y < maxOpen:
+	if colliding_entities > 0 and y < maxOpen:
 		y += delta_speed
 		get_node("door_inside").translate_object_local(Vector3(0, delta_speed, 0))
-	elif not open and y > delta_speed:
+	elif colliding_entities == 0 and y > delta_speed:
 		y -= delta_speed
 		get_node("door_inside").translate_object_local(Vector3(0, -delta_speed, 0))
 
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("player") or body.is_in_group("enemies"):
-		open = true
+		colliding_entities += 1
 
 
 func _on_Area_body_exited(body):
 	if body.is_in_group("player") or body.is_in_group("enemies"):
-		open = false
+		colliding_entities -= 1
